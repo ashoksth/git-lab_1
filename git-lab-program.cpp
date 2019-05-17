c16fa774f3a59b9191448d8760dd8d4a0e1c9efc
Ashok Shrestha <as521718@ohio.edu>
2019-05-17 19:14:20 -0400

    Initial commit

---
+++ git-lab-program.cpp
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cpp
+ *      Author: Ashok Shrestha
+ *        Date: May 17, 2019
+ * Description: I copied this code from homework sheet.
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
